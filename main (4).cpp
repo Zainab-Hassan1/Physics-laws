3585019f69691c944af8ff554bab58a55bbb56de
Zainab-Hassan1 <135728408+Zainab-Hassan1@users.noreply.github.com>
2023-06-06 18:52:55 +0300

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,90 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+/*
+                             team members:
+                             Zainab Almubarak
+                             Zainab Alnahab
+                             Zainab alrashed
+                             Hawra Algrash
+*/
+#include<iostream>
+#include<cmath> // added this library to use predefined functions
+using namespace std;
+const float G = 9.8f;
+float getPotential(float weight,float height){
+return weight*height*G ;
+}
+float getKinetic(float weight,float speed){
+return weight*pow(speed,2)/2 ; // used pow function instead of speed*speed
+}
+float getTotal(float weight,float speed,float height){
+return getPotential(weight,height)+getKinetic(weight,speed);
+}
+float getWork(float force,float distance){
+return force*distance ;
+}
+int main(){
+cout<<"Welcome To The Physics Program"<<endl<<endl;
+
+int choice ;
+
+ do{
+	    cout<<"Please Choose A Physical Law From The Menu Below:"<<endl<<"1.Potential Energy"<<endl<<"2.Kinetic Energy"<<endl<<"3.Total Energy"
+<<endl<<"4.Work"<<endl<<"5.Exit"<<endl<<"Your Choice : ";
+
+      
+
+    cin>>choice ;
+     float weight,speed,height,distance,result=0 ;
+     switch(choice){
+         case 1:
+           cout<<"Enter The Weight (kg) : ";
+          cin>>weight;
+         cout<<"Enter The Height (m) : ";
+         cin>>height;
+        result = getPotential(weight,height);
+           cout<<"Potential Energy = "<<result<<" J"<<endl;
+                break;
+        case 2:
+             cout<<"Enter The Weight (kg) : ";
+            cin>>weight;
+            cout<<"Enter The Speed (m/s) : ";
+            cin>>speed;
+         result = getKinetic(weight,speed);
+            cout<<"Kinetic Energy = "<<result<<" J"<<endl;
+            break;
+        case 3:
+                cout<<"Enter The Weight (kg) : ";
+             cin>>weight;
+            cout<<"Enter The Height (m) : ";
+             cin>>height;
+             cout<<"Enter The Speed (m/s) : ";
+             cin>>speed;
+             result = getTotal(weight,speed,height);
+             cout<<"Total Energy = "<<result<<" J"<<endl;
+              break;
+        case 4:
+            cout<<"Enter The Force (N) : ";
+            cin>>weight;
+            cout<<"Enter The Distance (m) : ";
+           cin>>distance;
+          result = getWork(weight,distance);
+           cout<<"Work = "<<result<<" J"<<endl;
+              break; 
+        case 5: // changed this case to 5 for exiting program
+                cout << "Exiting program" << endl;
+                break;
+        default:
+                cout << "Invalid choice" << endl;   
+       }
+           } while (choice <=4); // changed this condition to 4
+
+    return 0;
+}
+
